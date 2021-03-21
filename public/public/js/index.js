2ab57296cd0641937d09a650fcc19f49786099d3
retlaw7 <39586889+retlaw7@users.noreply.github.com>
2021-03-21 17:58:44 +0100

    small menu edit and trailerbtn fix

--- public/public/js/index.js
+++ public/public/js/index.js
@@ -1,14 +1,25 @@
 window.onload = () => {
     const openbtn = document.getElementById("openbtn");
     const closebtn = document.getElementById("closebtn");
+
     openbtn.addEventListener("click",openNav);
     closebtn.addEventListener("click",closeNav);
 
+
     function openNav() {
-      document.getElementById("menu").style.width = "250px";
+        document.getElementById("menu").style.width = "30%";
     }
 
     function closeNav() {
       document.getElementById("menu").style.width = "0";
     }
+
+    const trailerbtn = document.getElementById("trailerbtn");
+    trailerbtn.addEventListener("click", trailer);
+
+    function trailer() {
+                window.open(
+                  "https://youtu.be/t9wYu3lorFc", "_blank");
+    }
+
 }
