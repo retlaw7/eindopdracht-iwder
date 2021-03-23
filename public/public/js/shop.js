95a1df4ee245584e66ab0b835c297d5b7268cec8
retlaw7 <39586889+retlaw7@users.noreply.github.com>
2021-03-23 16:36:26 +0100

    shop toegevoegd, css gewijzigd
    shop pagina toegevoegd
    css gewijzigd om de css wat overzichtelijker te maken.

--- public/public/js/shop.js
+++ public/public/js/shop.js
@@ -2,9 +2,35 @@ window.onload = () => {
     const openbtn = document.getElementById("openbtn");
     const closebtn = document.getElementById("closebtn");
 
+    const buyButtons = document.getElementsByClassName("modalBtn");
+
+    for(var i = 0; i < buyButtons.length; i++){
+        buyButtons[i].addEventListener('click',modalOpen);
+    }
+
+
+
     openbtn.addEventListener("click",openNav);
     closebtn.addEventListener("click",closeNav);
 
+    var modal = document.getElementById("storeModal");
+    var modalOpen = document.getElementById("modalBtn");
+    var modalQuit = document.getElementById("modalQuit");
+
+
+    function modalOpen(){
+        modal.style.display = "block";
+    }
+
+    modalQuit.onclick = function(){
+        modal.style.display = "none";
+    }
+
+    window.onclick = function() {
+        if(event.target == modal){
+            modal.style.display = "none";
+        }
+    }
 
     function openNav() {
         document.getElementById("menu").style.width = "30%";
